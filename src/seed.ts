bd8276c85ebfb9715f5fd96cdb4989b3e5490392
Hanif Omar <hanif.omarr@gmail.com>
2025-11-26 23:55:57 +0800

    feat: Initialize Prisma ORM with a User model, migrations, and database configuration.

---
+++ src/seed.ts
@@ -0,0 +1,37 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+  const userAdmin = await prisma.user.upsert({
+    where: { email: "admin@example.com" },
+    update: {},
+    create: {
+      email: "admin@example.com",
+      password: "hashedPassword",
+      name: "Admin User",
+      role: "ADMIN",
+    },
+  });
+
+  const userResident = await prisma.user.upsert({
+    where: { email: "resident@example.com" },
+    update: {},
+    create: {
+      email: "resident@example.com",
+      password: "hashedPassword",
+      name: "Resident User",
+      role: "RESIDENT",
+    },
+  });
+
+  console.log({ userAdmin, userResident });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
